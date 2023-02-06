ca073df2e1518d5753a54bc742eb6c9349a5325a
Oleksandr Shevchuk <oleksandr.shevchuk@lanars.com>
2023-02-06 17:02:57 +0200

    Block function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import { Container, Typography } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
+import { useState } from "react";
 import AppForm from "./components/AppForm";
+import { AppWrapper } from "./components/AppWrapper";
+
+const yellowColor = "#FFEA20";
+const blueColor = "#8DCBE6";
 
 function App() {
+  const [background, setBackground] = useState(yellowColor);
+
+  const handleChangeBackground = () => {
+    const newBackground = background === yellowColor ? blueColor : yellowColor;
+    setBackground(newBackground);
+  };
+
   return (
-    <Container>
-      <Typography component="h2">Calculate Fibonacci Number</Typography>
-      <AppForm />
-    </Container>
+    <AppWrapper style={{ backgroundColor: background }}>
+      <Container>
+        <Button
+          sx={{ mb: 3 }}
+          variant="outlined"
+          onClick={handleChangeBackground}
+        >
+          Change background
+        </Button>
+        <Typography component="h2">Calculate Fibonacci Number</Typography>
+        <AppForm />
+      </Container>
+    </AppWrapper>
   );
 }
 
--- src/components/AppForm/styles/FormWrapper.tsx
+++ src/components/AppForm/styles/FormWrapper.tsx
@@ -2,5 +2,5 @@ import styled from "@emotion/styled";
 
 export const FormWrapper = styled.div`
   display: flex;
-  align-items: center;
-`
+  align-items: flex-end;
+`;
